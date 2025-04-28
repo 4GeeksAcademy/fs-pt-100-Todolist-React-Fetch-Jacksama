f1edc944ef254a513b9062055a9b86a1ad8514b6
Jacksson Sánchez <info@creativoz.es>
2025-04-28 00:35:50 +0000

    TodoList-fetch-jacksama

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TodoList } from "./TodoList.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [data, setData] = useState ()
+
+	let url ="https://playground.4geeks.com/todo/openapi.json"
+	let options = {
+		method: "GET",
+		body: JSON.stringify(TodoList),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	}//La estructura anterior varía un poco a la que explica Javi, pero me ha gutado más, así la entendí mucho mejor en la clase de Alexander @alesanchezr
+
+	useEffect(()=>{
+		fetch(url, options)
+	  
+	  .then(respuesta => {
+		  if (respuesta.status >=200 && respuesta.status <=300){
+			console.log("El request ha sido ejecutado correctamente")
+			return respuesta.json();
+		  }
+		  else{
+			console.log(`Hubo un error ${respuesta.status} en el request`)
+		  } 
+	  })
+
+	  .then(body => {
+		console.log(`Este es el body del request`, body)
+	  })
+
+	  .catch(error => {
+		  // Manejo de errores
+		  console.log(error);
+	  });
+	},[])
+	
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<TodoList/>
 		</div>
 	);
+
 };
+
 
 export default Home;
